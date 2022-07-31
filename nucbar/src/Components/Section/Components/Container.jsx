4c66a6324bb7299f34170536326e0e70b11c36b5
sebastian chaca <sebastian@sebastians-MacBook-Pro.local>
2022-07-31 19:48:43 -0300

    fix provider props

--- nucbar/src/Components/Section/Components/Container.jsx
+++ nucbar/src/Components/Section/Components/Container.jsx
@@ -1,9 +1,9 @@
 import { Flex } from '@chakra-ui/react';
 import React from 'react';
 import Provider from './Context/Provider';
-const Container = ({ children, response }) => {
+const Container = ({ children, response, loading, category }) => {
   return (
-    <Provider response={response}>
+    <Provider response={response} loading={loading} category={category}>
       <Flex alignContent="center" mx="30px" my="30px" flexDir="column">
         {children}
       </Flex>
